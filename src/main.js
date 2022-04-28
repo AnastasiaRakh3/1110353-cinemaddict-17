6b44778ff0b7b7066bfd715e8d59a09472af8c3c
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-04-28 16:43:33 +0300

    Correct name of modules, parameters and path

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import NatigationView from './view/nav-view.js';
 import FiltersView from './view/filters-view.js';
 import UserView from './view/user-view.js';
-import {render} from './render.js';
+import {render, RenderPosition} from './render.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const filmsPresenter = new FilmsPresenter();
 
-render(new UserView(), siteHeaderElement, 'beforeend');
-render(new NatigationView(), siteMainElement, 'afterbegin');
-render(new FiltersView(), siteMainElement, 'beforeend');
+render(new UserView(), siteHeaderElement);
+render(new NatigationView(), siteMainElement, RenderPosition.AFTERBEGIN);
+render(new FiltersView(), siteMainElement);
 
 filmsPresenter.init(siteMainElement);
 
--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -1,9 +1,9 @@
-import FilmsBlockView from '../view/films-block.view.js';
+import FilmsBlockView from '../view/films-block-view.js';
 import FilmsListView from '../view/films-list-view.js';
 import FilmsListContainerView from '../view/films-list-container-view.js';
 import LoadMoreButtonView from '../view/loadmore-button-view.js';
-import FilmCardView from '../view/filmcard-view.js';
-import {render} from '../render.js';
+import FilmCardView from '../view/film-card-view.js';
+import {render, RenderPosition} from '../render.js';
 
 export default class FilmsPresenter {
   filmsBlockComponent = new FilmsBlockView();
@@ -14,14 +14,14 @@ export default class FilmsPresenter {
   init = (filmsBlockContainer) => {
     this.filmsBlockContainer = filmsBlockContainer;
 
-    render(this.filmsBlockComponent, this.filmsBlockContainer, 'beforeend');
-    render(this.filmsListComponent, this.filmsBlockComponent.getElement(), 'afterbegin');
-    render(this.filmsListContainerComponent, this.filmsListComponent.getElement(), 'beforeend');
+    render(this.filmsBlockComponent, this.filmsBlockContainer);
+    render(this.filmsListComponent, this.filmsBlockComponent.getElement(), RenderPosition.AFTERBEGIN);
+    render(this.filmsListContainerComponent, this.filmsListComponent.getElement());
 
     for (let i = 0; i < 5; i++) {
-      render(new FilmCardView(), this.filmsListContainerComponent.getElement(), 'beforeend');
+      render(new FilmCardView(), this.filmsListContainerComponent.getElement());
     }
 
-    render(this.LoadMoreButtonComponent, this.filmsListComponent.getElement(), 'beforeend');
+    render(this.LoadMoreButtonComponent, this.filmsListComponent.getElement());
   };
 }
