9fec63219a6b4a952e57867610150f7b060f291c
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-05-05 15:47:34 +0300

    Write popup opening in films-presenter module

--- src/main.js
+++ src/main.js
@@ -3,23 +3,23 @@ import FiltersView from './view/filters-view.js';
 import UserView from './view/user-view.js';
 import FooterStatisticsView from './view/footer-statistics.js';
 import {render, RenderPosition} from './render.js';
-import FilmsPresenter from './presenter/films-presenter.js';
+import {FilmsPresenter, siteMainElement} from './presenter/films-presenter.js';
 import FilmCardsModel from './model/film-cards-model.js';
-import CommentsPresenter from './presenter/comments-presenter.js';
-import CommentsModel from './model/comments-model.js';
-import PopupPresenter from './presenter/popup-presenter.js';
-import PopupModel from './model/popup-model.js';
+// import CommentsPresenter from './presenter/comments-presenter.js';
+// import CommentsModel from './model/comments-model.js';
+// import PopupPresenter from './presenter/popup-presenter.js';
+// import PopupModel from './model/popup-model.js';
 
 const siteHeaderElement = document.querySelector('.header');
-const siteMainElement = document.querySelector('.main');
+// const siteMainElement = document.querySelector('.main');
 const footerStatisticsContainerElement = document.querySelector('.footer__statistics');
 
 const filmsPresenter = new FilmsPresenter();
 const filmCardsModel = new FilmCardsModel();
-const commentsPresenter = new CommentsPresenter();
-const commentsModel = new CommentsModel();
-const popupModel = new PopupModel();
-const popupPresenter = new PopupPresenter();
+// const commentsPresenter = new CommentsPresenter();
+// const commentsModel = new CommentsModel();
+// const popupModel = new PopupModel();
+// const popupPresenter = new PopupPresenter();
 
 render(new UserView(), siteHeaderElement);
 render(new NavigationView(), siteMainElement, RenderPosition.AFTERBEGIN);
@@ -28,7 +28,7 @@ render(new FooterStatisticsView(), footerStatisticsContainerElement);
 
 filmsPresenter.init(siteMainElement, filmCardsModel);
 
-popupPresenter.init(siteMainElement, popupModel);
-const commentsList = document.querySelector('.film-details__comments-list');
-commentsPresenter.init(commentsList, commentsModel);
+// popupPresenter.init(siteMainElement, popupModel);
+// const commentsList = document.querySelector('.film-details__comments-list');
+// commentsPresenter.init(commentsList, commentsModel);
 
--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -3,9 +3,13 @@ import FilmsListView from '../view/films-list-view.js';
 import FilmsListContainerView from '../view/films-list-container-view.js';
 import LoadMoreButtonView from '../view/loadmore-button-view.js';
 import FilmCardView from '../view/film-card-view.js';
+import PopupView from '../view/popup-view.js';
 import {render, RenderPosition} from '../render.js';
 
-export default class FilmsPresenter {
+const siteMainElement = document.querySelector('.main');
+const bodyElement = document.querySelector('body');
+
+class FilmsPresenter {
   #filmsBlockComponent = new FilmsBlockView();
   #filmsListComponent = new FilmsListView();
   #filmsListContainerComponent = new FilmsListContainerView();
@@ -25,9 +29,47 @@ export default class FilmsPresenter {
     render(this.#filmsListContainerComponent, this.#filmsListComponent.element);
 
     for (let i = 0; i < this.#filmCards.length; i++) {
-      render(new FilmCardView(this.#filmCards[i]), this.#filmsListContainerComponent.element);
+      this.#renderFilmCard(this.#filmCards[i]);
     }
 
     render(this.#loadMoreButtonComponent, this.#filmsListComponent.element);
   };
+
+  #renderFilmCard = (card) => {
+    const cardComponent = new FilmCardView(card);
+    const popupComponent = new PopupView(card);
+    const siteMainComponent = siteMainElement;
+
+    const openPopup = () => {
+      siteMainComponent.appendChild(popupComponent.element);
+      bodyElement.classList.add('hide-overflow');
+    };
+
+    const closePopup = () => {
+      siteMainComponent.removeChild(popupComponent.element);
+      bodyElement.classList.remove('hide-overflow');
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        closePopup();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    cardComponent.element.addEventListener('click', () => {
+      openPopup();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    popupComponent.element.querySelector('.film-details__close-btn').addEventListener('click', () => {
+      closePopup();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(cardComponent, this.#filmsListContainerComponent.element);
+  };
 }
+
+export {siteMainElement, FilmsPresenter};
